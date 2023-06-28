7a45f75f4afd48386c80015578a9c4c290224e3b
Keane Wesslius <Keanewesselius@gmail.com>
2023-06-27 20:54:57 -0700

    Modularized more of the commands on the bot
    Still have more modularization to go but almost finished, then all additional features will be added as modularized from their  creation.

---
+++ cogs/GDrive.py
@@ -0,0 +1,53 @@
+from discord.ext import commands
+from ..google import doc_creator
+
+
+
+class GDrive(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.google = doc_creator.Doc_Creator()
+
+
+    @commands.command()
+    #@slash.slash(name="createDoc", description="Creates a Google Doc file")
+    async def createDoc(self, ctx, *args):
+        if (args):
+            args = " ".join(args)
+            url = self.google.createDoc(args)
+            await ctx.send(url)
+        else:
+            url = self.google.createDoc()
+            await ctx.send(url)
+
+
+    @commands.command()
+    #@slash.slash(name="createSlide", description="Creates a Google Slide file")
+    async def createSlide(self, ctx, *args):
+        if (args):
+            args = " ".join(args)
+            url = self.google.createSlide(args)
+            await ctx.send(url)
+        else:
+            url = self.google.createSlide()
+            await ctx.send(url)
+
+
+    @commands.command()
+    #@slash.slash(name="createSheet", description="Creates a Google Sheet file")
+    async def createSheet(self, ctx, *args):
+        if (args):
+            args = " ".join(args)
+            url = self.google.createSheet(args)
+            await ctx.send(url)
+        else:
+            url = self.google.createSheet()
+            await ctx.send(url)
+
+
+
+
+
+async def setup(bot):
+    await bot.add_cog(GDrive(bot))
--- cogs/Misc.py
+++ cogs/Misc.py
@@ -4,7 +4,22 @@ class Misc(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.BOT_LINK = open("/home/pi/Python/A-Little-Help/botAccessLink.txt", "r").readline()
 
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f' Pong! {round(self.bot.latency * 1000)}ms')
+
+
+    @commands.command()
+    #@commands.is_owner()
+    async def info(self, ctx):
+        await ctx.send(f'Bot Info = {self.bot.user} \nUser ID = {ctx.author}\nGuild Name = {ctx.guild} \nGuild ID = {ctx.guild.id}\nUse !help for help with commands')
+
+    @commands.command()
+    #@slash.slash(name="addBot", description="Creates link to this add bot to a server")
+    async def addBot(self, ctx):
+        await ctx.send(self.BOT_LINK)
 
 async def setup(bot):
     await bot.add_cog(Misc(bot))
